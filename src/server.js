89cd02ce614d14e7ac6a20e5d9b515399f6035b3
Ryutakate <magegavladislav@gmail.com>
2025-07-18 16:11:34 +0300

    v-1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
-import contactsRouter from './controllers/contactsController.js';
+import contactsRouter from './routers/contacts';
 
 export const setupServer = () => {
     const app = express();
