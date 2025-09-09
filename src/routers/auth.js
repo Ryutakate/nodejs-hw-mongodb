c128a57e2c3ebb664d6305f01c88010f76efd2b5
Ryutakate <magegavladislav@gmail.com>
2025-09-09 20:25:44 +0300

    v-4

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,41 +1,10 @@
 import express from 'express';
-// import ctrl  from '../controllers/authController.js';
-import { registerSchema, loginSchema, sendResetEmailSchema, resetPasswordSchema } from '../schemas/authSchemas.js';
+import { registerSchema, loginSchema, sendResetEmailSchema, resetPasswordSchema } from '../schemas/authSchemas.js'; // Перевірте шлях
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../middlewares/validateBody.js';
-// import { resetPasswordController, sendResetEmailController } from '../controllers/authController.js';
 import * as ctrl from '../controllers/authController.js';
 
-
 const authRouter = express.Router();
-
-// authRouter.post(
-//     '/register',
-//     validateBody(registerSchema),
-//     ctrlWrapper(ctrl.register)
-// );
-
-// authRouter.post(
-//     '/login',
-//     validateBody(loginSchema),
-//     ctrlWrapper(ctrl.login)
-// );
-
-// authRouter.post('/refresh', ctrlWrapper(ctrl.refreshSession));
-// authRouter.post('/logout', ctrlWrapper(ctrl.logout));
-
-// authRouter.post(
-//     "/send-reset-email",
-//     validateBody(sendResetEmailSchema),
-//     ctrlWrapper(sendResetEmailController)
-// );
-
-// authRouter.post(
-//     '/reset-pwd',
-//     validateBody(resetPasswordSchema),
-//     ctrlWrapper(resetPasswordController)
-// );
-
 
 authRouter.post('/register', validateBody(registerSchema), ctrlWrapper(ctrl.register));
 authRouter.post('/login', validateBody(loginSchema), ctrlWrapper(ctrl.login));
@@ -43,6 +12,5 @@ authRouter.post('/refresh', ctrlWrapper(ctrl.refreshSession));
 authRouter.post('/logout', ctrlWrapper(ctrl.logout));
 authRouter.post('/send-reset-email', validateBody(sendResetEmailSchema), ctrlWrapper(ctrl.sendResetEmailController));
 authRouter.post('/reset-pwd', validateBody(resetPasswordSchema), ctrlWrapper(ctrl.resetPasswordController));
-
 
 export default authRouter;
--- src/schemas/authSchemas.js
+++ src/schemas/authSchemas.js
@@ -19,3 +19,7 @@ export const resetPasswordSchema = Joi.object({
     token: Joi.string().required(),
     password: Joi.string().min(6).required(),
 });
+
+export const emailSchema = Joi.object({
+    email: Joi.string().email().required(),
+})
