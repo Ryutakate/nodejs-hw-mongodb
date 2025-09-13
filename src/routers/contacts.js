5b27583d36be53c018642b90a8be53fef6d3409c
Ryutakate <magegavladislav@gmail.com>
2025-09-14 00:36:50 +0300

    v-5

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -1,57 +1,32 @@
-// import express from 'express';
-// import contactsController from '../controllers/contactsController.js';
-// import ctrlWrapper from '../utils/ctrlWrapper.js';
-// import validateBody from '../middlewares/validateBody.js';
-// import isValidId from '../middlewares/isValidId.js';
-// import authenticate from '../middlewares/authenticate.js';
-// import { createContactSchema, updateContactSchema } from '../schemas/contactSchemas.js';
-// import upload from '../middlewares/uploadMiddleware.js';
-
-
-// const router = express.Router();
-
-// router.use(authenticate);
-// router.get('/', ctrlWrapper(contactsController.getAllContacts));
-// router.get('/:contactId', isValidId, ctrlWrapper(contactsController.getContactById));
-// router.post('/', upload.single('photo'), validateBody(createContactSchema), ctrlWrapper(contactsController.createNewContact));
-// router.patch('/:contactId', isValidId, upload.single('photo'), validateBody(updateContactSchema), ctrlWrapper(contactsController.updateContact));
-// router.delete('/:contactId', isValidId, ctrlWrapper(contactsController.deleteContact));
-
-// export default router;
-
-
-
 import express from 'express';
-import contactsController from '../controllers/contactsController.js';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../middlewares/validateBody.js';
 import isValidId from '../middlewares/isValidId.js';
 import authenticate from '../middlewares/authenticate.js';
-import { createContactSchema, updateContactSchema } from '../schemas/contactSchemas.js';
+import { updateContactSchema } from '../schemas/contactSchemas.js';
 import upload from '../middlewares/uploadMiddleware.js';
+import { createContact, updateContactById as updateContact, getContactById, getAllContacts, deleteContactById as deleteContact } from '../services/contacts.js';
 
 const router = express.Router();
 
 router.use(authenticate);
 
-router.get('/', ctrlWrapper(async (req, res) => {
-    const { page = 1, perPage = 10, sortBy = 'name', sortOrder = 'asc', type, isFavourite } = req.query;
-    const result = await contactsController.getAllContacts({ page, perPage, sortBy, sortOrder, type, isFavourite });
-    res.status(200).json({
-        status: 200,
-        message: 'Contacts retrieved successfully',
-        data: {
-            totalItems: result.totalItems,
-            totalPages: result.totalPages,
-            page: Number(page),
-            limit: Number(perPage),
-            contacts: result.contacts,
-        },
-    });
-}));
+router.get('/', async (req, res, next) => {
+    try {
+        const { page = 1, perPage = 10, sortBy = 'name', sortOrder = 'asc', type, isFavourite, userId } = req.query;
+        const { contacts, totalItems, totalPages } = await getAllContacts({ page, perPage, sortBy, sortOrder, type, isFavourite, userId });
+        res.status(200).json({
+            status: 200,
+            message: 'Contacts retrieved successfully',
+            data: { totalItems, totalPages, page: Number(page), perPage: Number(perPage), contacts },
+        });
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.get('/:contactId', isValidId, ctrlWrapper(async (req, res) => {
-    const contact = await contactsController.getContactById(req.params.contactId);
+    const contact = await getContactById(req.params.contactId, req.user._id);
     if (!contact) {
         return res.status(404).json({
             status: 404,
@@ -65,19 +40,26 @@ router.get('/:contactId', isValidId, ctrlWrapper(async (req, res) => {
     });
 }));
 
-router.post('/', upload.single('photo'), validateBody(createContactSchema), ctrlWrapper(async (req, res) => {
-    const { file } = req;
-    const contact = await contactsController.createNewContact(req.body, file);
-    res.status(201).json({
-        status: 201,
-        message: 'Contact created',
-        data: { contact },
-    });
-}));
+router.post('/', upload.single('photo'), async (req, res, next) => {
+    try {
+        const { name, email, phoneNumber } = req.body;
+        const file = req.file;
+        const contact = await createContact({ name, email, phoneNumber, userId: req.user._id }, file);
+        res.status(201).json({
+            status: 201,
+            message: 'Contact created',
+            data: { contact },
+        });
+    } catch (error) {
+        next(error);
+    }
+});
 
 router.patch('/:contactId', isValidId, upload.single('photo'), validateBody(updateContactSchema), ctrlWrapper(async (req, res) => {
-    const { file } = req;
-    const contact = await contactsController.updateContact(req.params.contactId, req.body, file);
+    const { contactId } = req.params;
+    const { name, email, phoneNumber } = req.body;
+    const file = req.file;
+    const contact = await updateContact(contactId, { name, email, phoneNumber, userId: req.user._id }, file);
     if (!contact) {
         return res.status(404).json({
             status: 404,
@@ -91,15 +73,20 @@ router.patch('/:contactId', isValidId, upload.single('photo'), validateBody(upda
     });
 }));
 
-router.delete('/:contactId', isValidId, ctrlWrapper(async (req, res) => {
-    const result = await contactsController.deleteContact(req.params.contactId);
-    if (!result) {
-        return res.status(404).json({
-            status: 404,
-            message: 'Contact not found',
-        });
+router.delete('/:contactId', isValidId, async (req, res, next) => {
+    try {
+        const { contactId } = req.params;
+        const result = await deleteContact(contactId, req.user._id);
+        if (!result) {
+            return res.status(404).json({
+                status: 404,
+                message: 'Contact not found',
+            });
+        }
+        res.status(204).send();
+    } catch (error) {
+        next(error);
     }
-    res.status(204).send();
-}));
+});
 
 export default router;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,58 +1,108 @@
-import { Contact } from '../models/contactModel.js';
+// import { Contact } from '../models/contactModel.js';
+// import { uploadImageToCloudinary } from '../utils/uploadToCloudinary.js';
+
+// export const getAllContacts = async ({ page = 1, perPage = 10, sortBy = "name", sortOrder = "asc", type, isFavourite, userId }) => {
+//     const skip = (page - 1) * perPage;
+//     const sortOptions = { [sortBy]: sortOrder === "desc" ? -1 : 1 };
+//     const filter = { userId };
+
+//     if (type) {
+//         filter.contactType = type;
+//     }
+//     if (isFavourite !== undefined) {
+//         filter.isFavourite = isFavourite === "true";
+//     }
+
+//     const totalItems = await Contact.countDocuments(filter);
+
+//     const contacts = await Contact.find(filter)
+//         .sort(sortOptions)
+//         .skip(skip)
+//         .limit(Number(perPage));
+
+//     const totalPages = Math.ceil(totalItems / perPage);
+
+//     return {
+//         contacts,
+//         totalItems,
+//         totalPages,
+//     };
+// };
+
+// export const getContactById = async (id, userId) => {
+//     return await Contact.findOne({ _id: id, userId }); 
+// };
+
+// export const createContact = async (data, file) => {
+//     if (file) {
+//         const photoUrl = await uploadImageToCloudinary(file.buffer);
+//         data.photo = photoUrl;
+//     }
+//     const newContact = await Contact.create(data);
+//     return newContact;
+// };
+
+// export const updateContactById = async (id, data, file) => {
+//     if (file) {
+//         const photoUrl = await uploadImageToCloudinary(file.buffer);
+//         data.photo = photoUrl;
+//     }
+//     const updatedContact = await Contact.findByIdAndUpdate(id, data, { new: true });
+//     return updatedContact;
+// };
+
+// export const deleteContactById = async (id, userId) => {
+//     return await Contact.findOneAndDelete({ _id: id, userId });
+// };
+
+
+import { Contact } from '../models/contactModels.js';
 import { uploadImageToCloudinary } from '../utils/uploadToCloudinary.js';
 
-export const getAllContacts = async ({ page = 1, perPage = 10, sortBy = "name", sortOrder = "asc", type, isFavourite, userId }) => {
-    const skip = (page - 1) * perPage;
-    const sortOptions = { [sortBy]: sortOrder === "desc" ? -1 : 1 };
-    const filter = { userId };
+export const getAllContacts = async ({ page = 1, perPage = 10, sortBy = 'name', sortOrder = 'asc', type, isFavourite, userId }) => {
+  const skip = (page - 1) * perPage;
+  const sortOptions = { [sortBy]: sortOrder === 'desc' ? -1 : 1 };
+  const filter = { userId };
 
-    if (type) {
-        filter.contactType = type;
-    }
-    if (isFavourite !== undefined) {
-        filter.isFavourite = isFavourite === "true";
-    }
+  if (type) {
+    filter.contactType = type;
+  }
+  if (isFavourite !== undefined) {
+    filter.isFavourite = isFavourite === 'true';
+  }
 
-    const totalItems = await Contact.countDocuments(filter);
+  const totalItems = await Contact.countDocuments(filter);
+  const contacts = await Contact.find(filter)
+    .sort(sortOptions)
+    .skip(skip)
+    .limit(Number(perPage));
+  const totalPages = Math.ceil(totalItems / perPage);
 
-    const contacts = await Contact.find(filter)
-        .sort(sortOptions)
-        .skip(skip)
-        .limit(Number(perPage));
-
-    const totalPages = Math.ceil(totalItems / perPage);
-
-    return {
-        contacts,
-        totalItems,
-        totalPages,
-    };
+  return { contacts, totalItems, totalPages };
 };
 
 export const getContactById = async (id, userId) => {
-    return await Contact.findOne({ _id: id, userId }); 
+  return await Contact.findOne({ _id: id, userId });
 };
 
 export const createContact = async (data, file) => {
-    if (file) {
-        const photoUrl = await uploadImageToCloudinary(file.buffer);
-        data.photo = photoUrl;
-    }
-    const newContact = await Contact.create(data);
-    return newContact;
+  if (file) {
+    const photoUrl = await uploadImageToCloudinary(file.buffer);
+    data.photo = photoUrl;
+  }
+  const newContact = await Contact.create(data);
+  return newContact;
 };
 
-export const updateContactById = async (id, data, file) => {
-    if (file) {
-        const photoUrl = await uploadImageToCloudinary(file.buffer);
-        data.photo = photoUrl;
-    }
-    const updatedContact = await Contact.findByIdAndUpdate(id, data, { new: true });
-    return updatedContact;
+export const updateContact = async (id, data, file) => {
+  if (file) {
+    const photoUrl = await uploadImageToCloudinary(file.buffer);
+    data.photo = photoUrl;
+  }
+  const updatedContact = await Contact.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+  return updatedContact;
 };
 
-export const deleteContactById = async (id, userId) => {
-    return await Contact.findOneAndDelete({ _id: id, userId });
+export const deleteContact = async (id, userId) => {
+  return await Contact.findOneAndDelete({ _id: id, userId });
 };
-
-
